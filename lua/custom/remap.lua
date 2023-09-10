425594116f5abcac6b0ae78afb783fbd81c5f5f7
Arthur Wallendorff <hello@metaoist.io>
2023-09-10 15:35:50 +0200

    feature: do not display cmd line on line move

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -7,8 +7,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>qf", function()end)
 
 -- Moving lines around
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+
 
 -- Cursor centering
 vim.keymap.set("n", "J", "mzJ`z")
