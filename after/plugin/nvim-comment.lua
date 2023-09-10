467ee7bca05b8fde9089616f62df8ea702c67dcb
Arthur Wallendorff <hello@metaoist.io>
2023-09-10 18:01:15 +0200

    fix: 80-20 solution to multiline commenting

--- after/plugin/nvim-comment.lua
+++ after/plugin/nvim-comment.lua
@@ -2,10 +2,19 @@ local nvim_comment = require("nvim_comment")
 
 vim.keymap.set("n", "<C-/>", function()
     vim.cmd("CommentToggle")
-end)
+end,
+    { silent = true }
+)
+
+-- TODO find a way to stay in visual mode
+-- The problem is that the range is not updated until we leave visual mode
+-- so if we select more or less text, the command only applies to the
+-- original selection...
+vim.keymap.set("v", "<C-/>", ":'<,'>CommentToggle<Cr>",
+{ silent = true }
+)
 
 nvim_comment.setup({
     comment_empty = false,
-    -- Visual mode support
-    operator_mapping = "<C-/>"
+    create_mappings = false
 })
