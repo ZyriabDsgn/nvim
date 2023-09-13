3119f47fae1c0d065094b0e8b7467d048731914e
Arthur Wallendorff <hello@metaoist.io>
2023-09-13 22:17:10 +0200

    add live grep w/ args

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,19 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+local lga_actions = require("telescope-live-grep-args.actions")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-vim.keymap.set("n", "<leader>pf", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set("n", "<leader>gs", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set("n", "<leader>lg", function()
+    -- See `ripgrep`    
+    -- Grep client in all files under src, ignore tsx files: "client" --iglob src/** --iglob !*.tsx
+    -- Grep client in all files under src, ignore components: "client" --iglob src/** --iglob !**/components/**
+    -- Grep client in all files under any components directory: "client" --iglob **/components/**
+
+    telescope.extensions.live_grep_args.live_grep_args()
 end)
 
 vim.keymap.set("n", "<leader>nt", function()
--- lua/custom/lazy-nvim.lua
+++ lua/custom/lazy-nvim.lua
@@ -51,7 +51,13 @@ local plugins = {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.2",
-        dependencies = { "nvim-lua/plenary.nvim" }
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-live-grep-args.nvim" 
+        },
+        config = function()
+    	require("telescope").load_extension("live_grep_args")
+  	end
     },
     {
         "theprimeagen/harpoon"
