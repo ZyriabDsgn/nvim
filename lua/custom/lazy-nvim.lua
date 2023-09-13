9ddad1c16a12d748c289db1c1b7fe2ab4e1472f1
Arthur Wallendorff <hello@metaoist.io>
2023-09-13 12:08:06 +0200

    add gitgutter

--- lua/custom/lazy-nvim.lua
+++ lua/custom/lazy-nvim.lua
@@ -46,6 +46,9 @@ local plugins = {
         event = "VeryLazy"
     },
     {
+        "airblade/vim-gitgutter"
+    },
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.2",
         dependencies = { "nvim-lua/plenary.nvim" }
