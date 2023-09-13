269ae2f0c06d3234a7024db9e8d38fad4334b3d8
Arthur Wallendorff <hello@metaoist.io>
2023-09-13 07:34:48 +0200

    Only enables tranparency for Linux setup

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,5 @@
---     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-
-     -- xiyaowong/transparent.nvim
-     vim.cmd("TransparentEnable")
-     vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-
+if vim.loop.os_uname().sysname == "Linux" then
+    -- xiyaowong/transparent.nvim
+    vim.cmd("TransparentEnable")
+    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+end
