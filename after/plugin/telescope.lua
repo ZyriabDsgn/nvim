e770969be90cd479f98a7e84e37a587b1e0c5935
Arthur Wallendorff <hello@metaoist.io>
2023-09-15 21:58:48 +0200

    add telescope ui-select + ignore node_modules

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,8 +22,14 @@ vim.keymap.set("n", "<leader>nt", function()
     telescope.extensions.notify.notify()
 end)
 
-telescope.setup()
-require("telescope").load_extension("live_grep_args")
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+        }
+    }
+})
+telescope.load_extension("live_grep_args")
+telescope.load_extension("ui-select")
 -- telescope.load_extension("fzf")
 -- telescope.load_extension("zf-native")
--- telescope.load_extension("ui-select")
