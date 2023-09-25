9a209737f350c5f629ab664128f9b84ef69e44f0
Arthur Wallendorff <hello@metaoist.io>
2023-09-25 18:48:45 +0200

    tweak telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,25 +2,23 @@ local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 local lga_actions = require("telescope-live-grep-args.actions")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>tr", builtin.resume)
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>gf", builtin.git_files)
 
 vim.keymap.set("n", "<leader>gs", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
-vim.keymap.set("n", "<leader>lg", function()
-    -- See `ripgrep`
-    -- Grep client in all files under src, ignore tsx files: "client" --iglob src/** --iglob !*.tsx
-    -- Grep client in all files under src, ignore components: "client" --iglob src/** --iglob !**/components/**
-    -- Grep client in all files under any components directory: "client" --iglob **/components/**
+-- See `ripgrep`
+-- Grep client in all files under src, ignore tsx files: "client" --iglob src/** --iglob !*.tsx
+-- Grep client in all files under src, ignore components: "client" --iglob src/** --iglob !**/components/**
+-- Grep client in all files under any components directory: "client" --iglob **/components/**
+vim.keymap.set("n", "<leader>lg", telescope.extensions.live_grep_args.live_grep_args)
 
-    telescope.extensions.live_grep_args.live_grep_args()
-end)
 
-vim.keymap.set("n", "<leader>nt", function()
-    telescope.extensions.notify.notify()
-end)
+vim.keymap.set("n", "<leader>nt", telescope.extensions.notify.notify)
 
 telescope.setup({
     defaults = {
@@ -30,6 +28,7 @@ telescope.setup({
     },
 
 })
+
 telescope.load_extension("live_grep_args")
 -- telescope.load_extension("fzf")
 -- telescope.load_extension("zf-native")
