f7c559aee77dc53117ab0fad478b20cab2e3ddf9
Arthur Wallendorff <hello@metaoist.io>
2023-10-10 13:53:51 +0200

    transpareing time cleanup

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,7 @@
-local os_utils = require("utils.os")
+-- local os_utils = require("utils.os")
 
 -- if os_utils.is_linux() then
-    -- xiyaowong/transparent.nvim
-    vim.cmd("TransparentEnable")
-    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+-- xiyaowong/transparent.nvim
+vim.cmd("TransparentEnable")
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
 -- end
