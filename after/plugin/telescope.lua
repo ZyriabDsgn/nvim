bf3c692e87e04335b0f533dbe70c315b8be04752
Arthur Wallendorff <hello@metaoist.io>
2023-11-03 23:03:10 +0100

    tweak telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
-local lga_actions = require("telescope-live-grep-args.actions")
+-- local lga_actions = require("telescope-live-grep-args.actions")
 
 vim.keymap.set("n", "<leader>tr", builtin.resume)
 
@@ -16,7 +16,6 @@ end)
 -- Grep client in all files under src, ignore components: "client" --iglob src/** --iglob !**/components/**
 -- Grep client in all files under any components directory: "client" --iglob **/components/**
 vim.keymap.set("n", "<leader>lg", telescope.extensions.live_grep_args.live_grep_args)
-
 
 vim.keymap.set("n", "<leader>nt", telescope.extensions.notify.notify)
 
